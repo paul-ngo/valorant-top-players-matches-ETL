f68db34e1aa32083897cab4b93e3da8dc7591df0
paul-ngo <100042385+paul-ngo@users.noreply.github.com>
2022-03-19 00:19:16 -0700

    Update valMatchesEtl to include match_date field

--- valMatchesEtlScript.py
+++ valMatchesEtlScript.py
@@ -10,7 +10,7 @@ query1 = """
 dfPlayerList = pd.DataFrame(queryRds(query1), columns=['player_id', 'player_name'])
 
 dfMatchesId = m.scrapeMatchesHistory(dfPlayerList['player_id'], dfPlayerList['player_name'])
-dfMatchesId = dfMatchesId.drop_duplicates(subset=['MATCH_ID'])
+dfMatchesId = dfMatchesId.drop_duplicates(subset=['match_id'])
 
 query2 = """
     SELECT match_id
@@ -18,16 +18,16 @@ query2 = """
 """
 dfExistingMatches = pd.DataFrame(queryRds(query2), columns=['match_id'])
 
-dfMatchesId = dfMatchesId[~dfMatchesId['MATCH_ID'].isin(dfExistingMatches['match_id'])]
+dfMatchesId = dfMatchesId[~dfMatchesId['match_id'].isin(dfExistingMatches['match_id'])]
 
 insert1 = """
-    INSERT INTO matches (match_id, act_id, mode, match_url) VALUES (%s, %s, %s, %s)
+    INSERT INTO matches (match_id, act_id, mode, match_date, match_url) VALUES (%s, %s, %s, %s, %s)
 """
 val1 = list(dfMatchesId.itertuples(index=False, name=None))
 insertRds(insert1, val1)
 
 
-dfMatchesDetails = m.scrapeTeams(dfMatchesId['MATCH_ID'])
+dfMatchesDetails = m.scrapeTeams(dfMatchesId['match_id'])
 
 insert2 = """
     INSERT INTO matches_details (match_id, map, red_team, red_team_score, blue_team, blue_team_score) VALUES (%s, %s, %s, %s, %s, %s)
